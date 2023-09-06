3096bb6015fa3d5bd9bd6061fb5bcc578c87c134
Paola_G <perfectblue@live.com>
2023-09-06 12:21:30 -0700

    changed file names, finished User model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      matches: [/\w+@\w+.\w{2,3}+/, "Invalide email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtual: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function() {
+  return this.friends.length
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
